d154c6fa10678750e02411f94bcc942f7f8d4943
behrooz3500 <behrooz3500@yahoo.com>
2023-02-10 15:50:59 +0330

    add serializers for tasks and categories" ;

---
+++ core/todo/api/v1/serializers.py
@@ -0,0 +1,78 @@
+# rest
+from rest_framework import serializers
+
+# internal
+from todo.models import Todo, TaskCategory
+from accounts.models import Profile
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """ Serializer for TaskCategory model """
+
+    class Meta:
+        model = TaskCategory
+        fields = [
+            'id',
+            'name',
+        ]
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    """ Serializer for Todo model (tasks) """
+
+    has_been_updated = serializers.BooleanField(read_only=True)
+    relative_api_url = serializers.URLField(source="get_relative_api_url", read_only=True)
+    absolute_api_url = serializers.SerializerMethodField(method_name="get_absolute_url")
+
+    # Different ways to customize a fields shown value
+    # category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=TaskCategory.objects.all())
+    # category = CategorySerializer()
+
+    class Meta:
+        model = Todo
+        fields = (
+            "id",
+            "user",
+            "title",
+            "category",
+            "relative_api_url",
+            "absolute_api_url",
+            "completed",
+            "has_been_updated",
+            "created_date",
+            "updated_date",
+        )
+        read_only_fields = ['user']
+
+    def get_absolute_url(self, obj):
+        """ Retrieving absolute url for a given task."""
+
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
+    def to_representation(self, instance):
+        """ Managing the representaion of tasks in different views."""
+
+        request = self.context.get('request')
+        displayed_data = super().to_representation(instance)
+        displayed_data['category'] = CategorySerializer(instance.category).data
+        if request.parser_context.get('kwargs').get('pk'):
+            displayed_data.pop('relative_api_url', None)
+            displayed_data.pop('absolute_api_url', None)
+
+        return displayed_data
+
+    def create(self, validated_data):
+        """
+         Overriding create class to include aythenticated user info in
+        task creation form.
+        """
+
+        validated_data['user'] = Profile.objects.get(
+            user__id=self.context.get('request').user.id
+        )
+        return super().create(validated_data)
+
+
+
+
