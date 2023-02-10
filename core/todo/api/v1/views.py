48e9980bc19edcba941bad3fb7e487191a9c8eb3
behrooz3500 <behrooz3500@yahoo.com>
2023-02-10 15:51:22 +0330

    api views

---
+++ core/todo/api/v1/views.py
@@ -0,0 +1,43 @@
+# rest
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+# internal
+from .serializers import TodoSerializer, CategorySerializer
+from todo.models import Todo, TaskCategory
+from .permissions import IsOwnerOrReadOnly
+from .paginations import TaskListPagination
+from .filters import TaskFilter
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly ]
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+
+    search_filter = filters.SearchFilter
+    ordering_filter = filters.OrderingFilter
+    filter_backends = [DjangoFilterBackend, search_filter, ordering_filter]
+    filterset_class = TaskFilter
+    filterset_fields = ['category', 'completed', 'user']
+    search_fields = ['title']
+    ordering_fields = ['created_date', 'updated_date']
+    pagination_class = TaskListPagination
+
+    @action(methods=['GET'], detail=False)
+    def get_completed_tasks(self, request):
+        queryset = self.queryset.filter(completed=True)
+        serializer = TodoSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = CategorySerializer
+    queryset = TaskCategory.objects.all()
+
+
